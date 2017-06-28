c277608e976213ed2a61f98a3b1e7b6f94ca502a
Philip Bennett <bennettpa2@gmail.com>
2017-06-28 10:54:29 -0500

    added total function

--- lib.js
+++ lib.js
@@ -19,13 +19,29 @@ const listedPrice =
         ? listing.price
         : 0
 
+const total =
+  (listings, items) => {
+    let result = 0
+    for (let item in items) {
+      for (let listing in listings) {
+        result += listedPrice(listing, item.name)
+      }
+    }
+    return result
+  }
+
 /**
- * transform carts into an array of { customer, total }
- */
+* transform carts into an array of { customer, total }
+*/
 const calculateTotals =
   listings =>
     carts => {
-      // TODO
+      let result = []
+      for (let cart in carts) {
+        console.log(cart.customer)
+        result.push((cart.customer, total(listings, cart.items)))
+      }
+      return result
     }
 
 module.exports = {
